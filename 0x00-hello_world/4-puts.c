6c7347094f77b58528a8e87a2dfbbb3d30bc144b
Josnaz <atansiann@gmail.com>
2023-09-07 14:29:22 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - this is the main put function
- *
- * Return: always 0
+ * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return (0);
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
+
